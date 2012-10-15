1c438b711e9e405d5a3f0fb4288e0dd0d13413fb
Sonny Piers <sonny.piers@gmail.com>
2012-10-15 15:38:19 +0200

    many changes

---
+++ plugins/SCRAM-SHA-1.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+  Copyright (c) 2012, Sonny Piers <sonny at fastmail dot net>
+
+  Permission to use, copy, modify, and/or distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/*
+References:
+  Extensible Messaging and Presence Protocol (XMPP): Core - SASL Negotiation
+      http://xmpp.org/rfcs/rfc6120.html#sasl
+  Simple Authentication and Security Layer (SASL)
+      http://tools.ietf.org/html/rfc4422
+*/
+
+Lightstring.plugins['SCRAM-SHA-1'] = {
+  handlers: {
+    'mechanisms': function (stanza) {
+      if(stanza.mechanisms.indexOf('SCRAM-SHA-1') === -1)
+        return;
+
+      this.send(
+        "<auth xmlns='urn:ietf:params:xml:ns:xmpp-sasl'" +
+             " mechanism='SCRAM-SHA-1'/>"
+      );
+    },
+    'success': function (stanza) {
+      this.send(
+        "<stream:stream to='" + this.jid.domain + "'" +
+                      " xmlns='jabber:client'" +
+                      " xmlns:stream='http://etherx.jabber.org/streams'" +
+                      " version='1.0'/>"
+      );
+    },
+    'features': function (stanza) {
+      var Conn = this;
+      //TODO check if bind supported
+      var bind =
+        "<iq type='set' id='"+Lightstring.newId('sendiq:')+"'>" +
+          "<bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/>" +
+        "</iq>";
+
+      this.send(
+        bind,
+        //Success
+        function(stanza) {
+          //Session http://xmpp.org/rfcs/rfc3921.html#session
+          Conn.jid = new Lightstring.JID(stanza.DOM.textContent);
+
+          Conn.send(
+            "<iq type='set' id='"+Lightstring.newId('sendiq:')+"'>" +
+              "<session xmlns='urn:ietf:params:xml:ns:xmpp-session'/>" +
+            "</iq>",
+            function() {
+              Conn.emit('connected');
+            }
+          );
+        },
+        //Error
+        function(stanza) {
+          //TODO: Error?
+        }
+      );
+    }
+  }
+};
